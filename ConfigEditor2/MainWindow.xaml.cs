9266fcd7c06dc8728659bfa915b639a1f0e2b7a6
MrKos <kostya_sniper@inbox.ru>
2013-08-15 17:35:40 +0400

    check pls)

--- ConfigEditor2/MainWindow.xaml.cs
+++ ConfigEditor2/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Xml.Linq;
 using ConfigEditor2.Classes;
@@ -16,46 +18,57 @@ namespace ConfigEditor2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ConfigElement _path = new ConfigElement()
-        {
-            Key = "",
-            Value = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments)
-        };
+        private List<ConfigElement> _path = new List<ConfigElement>();
         private string SelectedDepartment = "0";
         
         public MainWindow()
         {
             InitializeComponent();
-            GetSelectedClientPath(SelectedDepartment);
+            for (int i = 0; i < 5; i++)
+            {
+                _path.Add(new ConfigElement(){Key = i.ToString(),Value = ""});
+            }
+            LoadConfig();
+            DepartmentSelect.SelectedIndex = 0;
+            FillPathTextBox(SelectedDepartment);
+            
         }
 
-        private void GetSelectedClientPath(string departNumber)
+        private void LoadConfig()
         {
             if (File.Exists("ERZDCom.config"))
             {
-                var loadConfig = SimpleConfig.LoadConfig("ERZDCom.config");
-                var findResult = loadConfig.ConfigElements.Find(element => element.Key.Equals(departNumber));
-                if (findResult != null)
+                try
                 {
-                    _path.Value = findResult.Value;
-                    PathTextBox.Text = _path.Value;
+
+                    var loadConfig = SimpleConfig.LoadConfig("ERZDCom.config");
+                    if (loadConfig == null) return;
+                    foreach (ConfigElement config in loadConfig.ConfigElements)
+                    {
+                        _path.Find(p => p.Key.Equals(config.Key)).Value = config.Value;
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    PathTextBox.Text = string.Empty;
+                    MessageBox.Show("Ошибка при откритии файла конфигурации: " + ex);
                 }
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FillPathTextBox(string departNumber)
+        {
+            PathTextBox.Text = _path.Find(p=>p.Key.Equals(SelectedDepartment)).Value ;
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
         {
             var folderDlg = new System.Windows.Forms.FolderBrowserDialog();
             folderDlg.ShowDialog();
             if (!string.IsNullOrEmpty(folderDlg.SelectedPath))
             {
-                _path.Value = folderDlg.SelectedPath;
+                _path.Find(p=>p.Key.Equals(SelectedDepartment)).Value = folderDlg.SelectedPath;
             }
-            PathTextBox.Text = _path.Value;
+            PathTextBox.Text = _path.Find(p=>p.Key.Equals(SelectedDepartment)).Value;
         }
 
         private void ErrorResult()
@@ -64,24 +77,33 @@ namespace ConfigEditor2
             Application.Current.Shutdown();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Save_Click(object sender, RoutedEventArgs e)
         {
             if (_path != null)
             {
-                SimpleConfig res = new SimpleConfig();
-                _path.Key = SelectedDepartment;
-                res.ConfigElements.Add(_path);
-                SimpleConfig.SaveConfig(res, "ERZDCom.config");
-                MessageBox.Show("Конфигурация сохранена!");
-                //Application.Current.Shutdown();
+                try
+                {
+                    SimpleConfig res = new SimpleConfig();
+                    foreach (ConfigElement configElement in _path)
+                    {
+                        res.ConfigElements.Add(configElement);
+                    }
+
+                    SimpleConfig.SaveConfig(res, "ERZDCom.config");
+                    MessageBox.Show("Конфигурация сохранена!");
+                    //Application.Current.Shutdown();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при сохранении в файл конфигурации: " + ex);
+                }
             }
         }
 
         private void DepartmentSelect_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             SelectedDepartment = DepartmentSelect.SelectedIndex.ToString(CultureInfo.InvariantCulture);
-            GetSelectedClientPath(SelectedDepartment);
+            FillPathTextBox(SelectedDepartment);
         }
-
     }
 }
